b0ec049b5f85d4cdfc61efa5e2b125a5725b2abb
tyg3rpro <32470566+tyg3rpro@users.noreply.github.com>
2024-08-05 02:51:28 -0400

    Update build.sh
    use newest versions of build tools

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-11 build
+[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b main build
 
 # Unpack python3-evdev to overlay until it is added to rootfs
 [ -f python3-evdev_1.3.0+dfsg-1build1_arm64.deb ] || wget http://ports.ubuntu.com/ubuntu-ports/pool/universe/p/python-evdev/python3-evdev_1.3.0+dfsg-1build1_arm64.deb
